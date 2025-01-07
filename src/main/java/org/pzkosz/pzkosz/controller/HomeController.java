2ea58254ab88160de8232bd33fbe96002149a3ab
KajtekWisniewski <k.wisniewski.603@studms.ug.edu.pl>
2025-01-07 17:54:37 +0100

    updated home page

--- src/main/java/org/pzkosz/pzkosz/controller/HomeController.java
+++ src/main/java/org/pzkosz/pzkosz/controller/HomeController.java
@@ -1,6 +1,7 @@
 package org.pzkosz.pzkosz.controller;
 
 import org.pzkosz.pzkosz.model.Team;
+import org.pzkosz.pzkosz.service.MatchStatsService;
 import org.pzkosz.pzkosz.service.TeamService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,13 +14,17 @@ import java.util.List;
 public class HomeController {
 
     private final TeamService teamService;
+    private final MatchStatsService matchStatsService;
 
-    public HomeController(TeamService teamService) {
+    public HomeController(TeamService teamService, MatchStatsService matchStatsService) {
+
         this.teamService = teamService;
+        this.matchStatsService = matchStatsService;
     }
 
     @GetMapping("/")
     public String viewAllTeams(Model model) {
+        matchStatsService.updateAllTeamsWinsAndLosses();
         List<Team> teams = teamService.getAllTeams();
 
         List<Team> sortedTeams = teams.stream()
